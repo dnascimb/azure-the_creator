193f2b786da17d2632c2a9a30490143d80e7b6a6
Dan Nascimbeni <dnascimb@gmail.com>
2015-09-01 01:34:48 +0000

    enabled deletion of resource groups

--- reapor.py
+++ reapor.py
@@ -7,8 +7,9 @@ import redis
 import logging
 import logging.handlers
 import sys
+import subprocess
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('reapor')
 LOG_FILENAME = 'the_creator.log'
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -20,28 +21,28 @@ handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
 handler.setFormatter(formatter)
 log.addHandler(handler)
 
-log.debug("Don't fear the reapor")
+log.info("Don't fear the reapor")
 
 try:
-
 	r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 	# determine which entries have met or exceeded their TTL
 	for key in r.scan_iter():
-		log.debug('key: ' + key.decode('utf-8'))
-		log.debug('value: ' + r.get(key.decode('utf-8')).decode('utf-8'))
-		obj = r.get(key.decode('utf-8')).decode('utf-8')
+		dkey = key.decode('utf-8')
+		log.debug('key: ' + dkey)
+		log.debug('value: ' + r.get(dkey).decode('utf-8'))
+		obj = r.get(dkey).decode('utf-8')
 		jobj = json.loads(obj)
 		ttl = datetime.strptime(jobj['ttl'], "%Y-%m-%d %H:%M:%S.%f")
-		log.debug(ttl)
+		
 		if(ttl <= datetime.utcnow()):
 			# remove appropriate candidates
-			log.info('deleting ' + key.decode('utf-8'))
-			#bashCommand = "delete.bat " + key
-			#process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd='c:/github/azure-the_creator/')
-			#output = process.communicate()[0]
+			log.info('deleting ' + dkey)
+			bashCommand = "delete.bat " + dkey
+			process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd='c:/github/azure-the_creator/')
+			output = process.communicate()[0]
 			# way to interigate response for failure?
-			#r.delete(key)
+			r.delete(dkey)
 except:
 	e = sys.exc_info()
 	log.error(e)
