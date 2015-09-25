4bf15a4b76e7a33e524009689fd3730f42757ca1
Dan Nascimbeni <dnascimb@gmail.com>
2015-09-25 01:18:15 +0000

    refactored to use central storage group; generating guid for component names; persist json parameter file for each deployment

--- creator.py
+++ creator.py
@@ -35,8 +35,7 @@ def greetings():
 @app.route("/resources", methods=['POST'])
 def create():
 	randName = str(uuid.uuid4()) # generate unique name for the resources
-	bashCommand = "create.bat " + randName
-	data = {
+	reapor_data = {
 		'resource_group_name' : randName,
    		'created_at' : ''+datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
    		'ttl' : ''+(datetime.utcnow() + timedelta(minutes=30)).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
@@ -44,13 +43,32 @@ def create():
 
 	#create resources and save audit to redis
 	try:
+		#generate random names for everything
+		param_file = open('azuredeploy.parameters.json', "r")    
+		data = json.load(param_file)
+		randName_condensed = randName.replace("-", "")
+		randName_condensed = randName_condensed[:12]
+		data["publicDnsName1"]["value"] = "kvm1-" + randName
+		data["publicDnsName2"]["value"] = "kvm2-" + randName
+		data["publicDnsName3"]["value"] = "kvm3-" + randName
+		data["vmName1"]["value"] = "vm1" + randName_condensed
+		data["vmName2"]["value"] = "vm2" + randName_condensed
+		data["vmName3"]["value"] = "vm3" + randName_condensed
+		output_filename = randName + "-parameters.json"
+		param_out = open(output_filename,"w")
+		param_out.write(json.dumps(data))
+		param_out.close()
+
+		bashCommand = "create.bat " + randName + " " + output_filename
+
+
 		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd='c:/github/azure-the_creator/')
 		output = process.communicate()[0]
 		str_output = output.decode('UTF-8')
 		log.info("result of running azure cli:" + str_output)
 		if('Succeeded' in str_output):
 			r = redis.StrictRedis(host='localhost', port=6379, db=0)
-			r.set(randName, json.dumps(data))
+			r.set(randName, json.dumps(reapor_data))
 			log.info("created resource " + str(r.get(randName)));
 		else:
 			log.error("Resource Group creation did not return success")
