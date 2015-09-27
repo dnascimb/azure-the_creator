f9ad44c16a75b1a5ece7961e1f2aee523380c81e
Dan Nascimbeni <dnascimb@gmail.com>
2015-09-27 16:37:56 -0700

    added shell versions of bat files

---
+++ create.sh
@@ -0,0 +1,8 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Creates a resource group of 3 machines based off an existing image, and given a name as a parameter (no spaces)
+:: 
+:: Example:
+:: create.bat K83jd-ke83hf-kE308-23Zlsie-03kd1
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+azure group create --json "%1" "West US" -f azuredeploy.json -d "testDeploy" -e %2
