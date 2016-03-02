3609675a48255c1bfe4e08b4f2fedc245cd628fc
paultar <ptarantino@kollective.com>
2016-03-02 03:46:53 +0000

    Some phase 2 stuff

--- marketplace_stuff/FixDesktop.bat
+++ marketplace_stuff/FixDesktop.bat
@@ -1,2 +1,6 @@
+@echo off
+REG ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v Window_Placement /t REG_BINARY /d 2c0000000000000001000000ffffffffffffffffffffffffffffffff01000000E60100007F02000010040000 /f
+cls
+exit
 
-REG ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v Window_Placement /t REG_BINARY /d 2c0000000000000001000000ffffffffffffffffffffffffffffffff510200000d00000062070000fd030000 /f
+
