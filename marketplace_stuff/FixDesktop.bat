a0a5df52fc66a81b9351e41383c2e3f42ec865ea
paultar <ptarantino@kollective.com>
2016-03-10 18:31:33 +0000

    Phase 2 stuff

--- marketplace_stuff/FixDesktop.bat
+++ marketplace_stuff/FixDesktop.bat
@@ -1,5 +1,7 @@
 @echo off
-REG ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v Window_Placement /t REG_BINARY /d 2c0000000000000001000000ffffffffffffffffffffffffffffffff01000000E60100007F02000010040000 /f
+REG ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v Window_Placement /t REG_BINARY /d 2c0000000000000001000000ffffffffffffffffffffffffffffffffffffffffe80100008002000012040000 /f
+REG ADD "HKLM\Software\Wow6432Node\Kontiki\kontiki" /v dmWidth /t REG_DWORD /d 640 /f
+REG ADD "HKLM\Software\Wow6432Node\Kontiki\kontiki" /v dmHeight /t REG_DWORD /d 500 /f
 cls
 exit
 
